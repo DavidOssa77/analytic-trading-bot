176d900e2b8fd0b0242e96aaaf2beb2ded2f200c
David <davidossa45@gmail.com>
2026-09-09 20:04:58 -0500

    agrega  contracts, data, analytics y forecasting

--- src/__init__.py
+++ src/__init__.py
@@ -3,7 +3,10 @@
 from .contracts import Params, Window
 from .analytics import log_returns, sample_stats, annualize, drawdown, descriptives, diagnostics
 from .forecasting import fit, cumulative_moments, terminal, path, walk_forward
+from .data import load_fixture, fetch_prices, resample_prices, coverage, align
 
-__all__ = ["Params", "Window", "log_returns", "sample_stats", "annualize", "drawdown",
-            "descriptives", "diagnostics","fit", "cumulative_moments", "terminal", "path", 
-            "walk_forward"]
+__all__ = [
+        "Params", "Window", "load_fixture", "fetch_prices", "resample_prices", "coverage", 
+        "align", "log_returns", "sample_stats", "annualize", "drawdown", "descriptives", 
+        "diagnostics", "fit", "cumulative_moments", "terminal", "path", "walk_forward"
+            ]
