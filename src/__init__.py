2380634845de2f0cb3753b02b321b1db96261f0d
David <davidossa45@gmail.com>
2026-09-12 10:32:16 -0500

    Expone las cuatro funciones de risk_rules en el API del paquete

--- src/__init__.py
+++ src/__init__.py
@@ -4,9 +4,10 @@ from .contracts import Params, Window
 from .analytics import log_returns, sample_stats, annualize, drawdown, descriptives, diagnostics
 from .forecasting import fit, cumulative_moments, terminal, path, walk_forward
 from .data import load_fixture, fetch_prices, resample_prices, coverage, align
+from .risk_rules import var, levels, terminal_probs, signal_gate
 
 __all__ = [
         "Params", "Window", "load_fixture", "fetch_prices", "resample_prices", "coverage", 
         "align", "log_returns", "sample_stats", "annualize", "drawdown", "descriptives", 
-        "diagnostics", "fit", "cumulative_moments", "terminal", "path", "walk_forward"
-            ]
+        "diagnostics", "fit", "cumulative_moments", "terminal", "path", "walk_forward", 
+        "var", "levels", "terminal_probs", "signal_gate"]
