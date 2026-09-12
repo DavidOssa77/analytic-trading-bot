b5373e24d75ddc8a80391daad3b04e2c3699cb45
David <davidossa45@gmail.com>
2026-09-12 11:35:15 -0500

    Creación de summary y preselect

--- src/__init__.py
+++ src/__init__.py
@@ -5,9 +5,10 @@ from .analytics import log_returns, sample_stats, annualize, drawdown, descripti
 from .forecasting import fit, cumulative_moments, terminal, path, walk_forward
 from .data import load_fixture, fetch_prices, resample_prices, coverage, align
 from .risk_rules import var, levels, terminal_probs, signal_gate
+from .compare import summary, preselect
 
 __all__ = [
         "Params", "Window", "load_fixture", "fetch_prices", "resample_prices", "coverage", 
         "align", "log_returns", "sample_stats", "annualize", "drawdown", "descriptives", 
         "diagnostics", "fit", "cumulative_moments", "terminal", "path", "walk_forward", 
-        "var", "levels", "terminal_probs", "signal_gate"]
+        "var", "levels", "terminal_probs", "signal_gate", "summary", "preselect"]
