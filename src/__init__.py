92a5aa9b66bacb4ebca5e215fc7ffe5c3e79e292
David <davidossa45@gmail.com>
2026-09-12 18:56:38 -0500

    Agregar analyze del pipeline a __all__

--- src/__init__.py
+++ src/__init__.py
@@ -6,9 +6,9 @@ from .forecasting import fit, cumulative_moments, terminal, path, walk_forward
 from .data import load_fixture, fetch_prices, resample_prices, coverage, align
 from .risk_rules import var, levels, terminal_probs, signal_gate
 from .compare import summary, preselect
-
+from .pipeline import analyze
 __all__ = [
         "Params", "load_fixture", "fetch_prices", "resample_prices", "coverage", "align", 
         "log_returns", "sample_stats", "annualize", "drawdown", "descriptives", 
         "diagnostics", "fit", "cumulative_moments", "terminal", "path", "walk_forward", 
-        "var", "levels", "terminal_probs", "signal_gate", "summary", "preselect"]
+        "var", "levels", "terminal_probs", "signal_gate", "summary", "preselect", "analyze"]
